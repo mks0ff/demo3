9263bad4da751e9f53a7b8f7dd9cae348cc5b729
reeyuk <sofiane.laouini@gmail.com>
2019-03-21 14:23:27 +0100

    add controller

---
+++ src/main/java/com/dailymed/demo3/InfoController.java
@@ -0,0 +1,16 @@
+package com.dailymed.demo3;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/hello")
+public class InfoController {
+
+    @GetMapping
+    public @ResponseBody String sayHello(@RequestParam(required = false) String name) {
+        return Objects.isNull(name) ? "Hello : Stranger !": "Hello : " + name;
+    }
+
+}
